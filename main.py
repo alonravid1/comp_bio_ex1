8bdcc1ef5fc0cf13a31d4a885f0c74ce66490cac
alonravid1 <alonravid10@gmail.com>
2023-04-25 16:46:22 +0300

    creates graphs

--- main.py
+++ main.py
@@ -21,6 +21,24 @@ def check_sum(s1, s2, s3, s4):
         s1 += diff
         return s1, s2, s3, s4
         
+
+def draw_graph(stats_samples, results, labels, title):
+    x = np.arange(stats_samples)
+    x = x*5
+    colors = []
+
+    for i in range(stats_samples):
+        colors.append('#%06X' % np.random.randint(0, 0xFFFFFF))
+    graph = plt.figure()
+    for i in range(len(results)):
+        plt.plot(x, results[i], color=colors[i], label=labels[i])
+    plt.xlabel("Iteration Number")
+    plt.ylabel("Percentage Spread")
+    plt.legend()
+    plt.title(title)
+    
+
+     
 if __name__ == '__main__':
     # gui = Gui.Gui()
     # gui.start()
@@ -44,47 +62,59 @@ if __name__ == '__main__':
    
 
     # sim = Simulation(0.7, 2, 15, 0.7, 0.15, 0.1, 0.05)
-    repeats = 1
+    repeats = 15
+    stats_samples = 20
+    stats_sr = iterations//stats_samples
     distributions = [
-        [0.7, 0.15, 0.1, 0.05], [0.6, 0.15, 0.1, 0.15]
-        # [0.6, 0.15, 0.15, 0.1], [0.5, 0.2, 0.15, 0.15],
-        # [0.5, 0.25, 0.15, 0.1], [0.4, 0.25, 0.2, 0.15],
-        # [0.4, 0.2, 0.2, 0.2], [0.4, 0.3, 0.2, 0.1],
-        # [0.3, 0.25, 0.2, 0.15], [0.3, 0.25, 0.25, 0.2]
+        [0.7, 0.15, 0.1, 0.05], [0.6, 0.15, 0.1, 0.15],
+        [0.6, 0.15, 0.15, 0.1], [0.5, 0.2, 0.15, 0.15],
+        [0.5, 0.25, 0.15, 0.1], [0.4, 0.25, 0.2, 0.15],
+        [0.4, 0.2, 0.2, 0.2], [0.4, 0.3, 0.2, 0.1],
+        [0.3, 0.25, 0.2, 0.15], [0.3, 0.25, 0.25, 0.2]
         ]
     
-    dist_results = np.zeros((len(distributions), 20))
+    dist_results = np.zeros((len(distributions), stats_samples))
+    dist_labels = []
     for dist in range(len(distributions)):
-        average_spread = np.zeros(20)
+        average_spread = np.zeros(stats_samples)
         distributions[dist] = check_sum(*distributions[dist])
         sim_values = [p, l, *distributions[dist], iterations]
 
         for i in range(repeats):
             sim = Sim.Simulation(*sim_values)
-            frames, stats = sim.run(preprocess=True)
-            for i in range(0, 20):
+            frames, stats = sim.run(preprocess=True, stats_sr=stats_sr)
+            for i in range(0, stats_samples):
                 average_spread[i] += stats[i]
 
         average_spread = average_spread/repeats
         dist_results[dist] = average_spread
+
+        dist_labels.append(f'distribution: {dist}; STD: {round(np.std(dist_results[dist]),3)}')
     print(dist_results)
 
-    limit_results = np.zeros((1, 20))
-    for l in range(2,3):
-        average_spread = np.zeros(20)
+    limit_results = np.zeros((2, stats_samples))
+    limit_labels = []
+    upper_limit = 10
+    for l in range(2,upper_limit):
+        average_spread = np.zeros(stats_samples)
         dist = [0.7, 0.15, 0.1, 0.05]
         sim_values = [p, l, *dist, iterations]
 
         for i in range(repeats):
             sim = Sim.Simulation(*sim_values)
-            frames, stats = sim.run(preprocess=True)
-            for i in range(0, 20):
+            frames, stats = sim.run(preprocess=True, stats_sr=stats_sr)
+            for i in range(0, stats_samples):
                 average_spread[i] += stats[i]
 
         average_spread = average_spread/repeats
-        limit_results[l-1] = average_spread
+        limit_results[l-2] = average_spread
+        limit_labels.append(f"L={l}")
     print(limit_results)
 
-x = np.arange(20)
-plt.plot(x, dist_results, color='b', label='distributions')
-plt.plot(x, limit_results, color='r', label='distributions')
+    fig, ax = plt.subplots(1,2)
+    draw_graph(stats_samples, dist_results, dist_labels, "Distributions Spread Rate")
+    plt.savefig("Distributions.png")
+    draw_graph(stats_samples, limit_results, limit_labels, "L Spread Rates")
+    plt.savefig("L.png")
+
+
